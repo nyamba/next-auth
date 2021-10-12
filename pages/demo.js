b5d6a52466914a6dd22cce447a7e508455343628
Nyambayar Turbat <t.nyambayar@gmail.com>
2021-10-12 16:04:22 +0800

    email

--- pages/demo.js
+++ pages/demo.js
@@ -1,10 +1,14 @@
+import {useState} from 'react'
 import Layout from '../components/layout'
 import FacebookLogin from 'react-facebook-login'
 
 
 export default function Page () {
+  const [email, setEmail] = useState(null)
+
   
   const responseFacebook = (response) => {
+    const {email} = response;
     console.log(response);
   }
 
@@ -12,6 +16,7 @@ export default function Page () {
   return (
     <Layout>
       <h1>Demontsrate Social login</h1>
+      {email ? <p>{email}</p>: ''}
       <FacebookLogin
         appId="389804239452408"
         fields="name,email,picture"
